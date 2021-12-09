5e0f9ee93b2269a02137b4b24ddbad517fae777c
Jie <jieren9806@gmail.com>
2021-12-09 16:05:22 +0800

    checked by cpplint

--- include/problem/HEntrance.h
+++ include/problem/HEntrance.h
@@ -6,34 +6,37 @@
 **/
 
 #pragma once
-#include <vertex/BaseVertex.h>
+#include <set>
+#include <map>
+#include <vector>
+#include <memory>
+#include "vertex/BaseVertex.h"
 
 namespace MegBA {
-    struct ProblemOption {
-        bool useSchur{true};
-        int worldSize{1};
-        device_t device{CUDA_t};
-        int N{-1};
-        int64_t nElm{-1};
-    };
+struct ProblemOption {
+  bool useSchur{true};
+  int worldSize{1};
+  device_t device{CUDA_t};
+  int N{-1};
+  int64_t nElm{-1};
+};
 
-    template<typename T>
-    struct SchurHEntrance {
-        // first is camera
-        using BlockRow = std::set<BaseVertex<T> *>;
-        using BlockMatrix = std::map<BaseVertex<T> *, BlockRow>;
-        std::array<BlockMatrix, 2> nra;
-        using BlockRowRA = std::vector<BaseVertex<T> *>;
-        using BlockMatrixRA = std::vector<BlockRowRA>;
-        std::array<BlockMatrixRA, 2> ra;
-        std::array<std::unique_ptr<int[]>, 2> csrRowPtr;
-        std::array<std::unique_ptr<int[]>, 2> csrColInd;
-        std::size_t counter{0};
-        std::array<int, 2> dim{};
-        std::size_t nnzInE{};
+template <typename T> struct SchurHEntrance {
+  // first is camera
+  using BlockRow = std::set<BaseVertex<T> *>;
+  using BlockMatrix = std::map<BaseVertex<T> *, BlockRow>;
+  std::array<BlockMatrix, 2> nra;
+  using BlockRowRA = std::vector<BaseVertex<T> *>;
+  using BlockMatrixRA = std::vector<BlockRowRA>;
+  std::array<BlockMatrixRA, 2> ra;
+  std::array<std::unique_ptr<int[]>, 2> csrRowPtr;
+  std::array<std::unique_ptr<int[]>, 2> csrColInd;
+  std::size_t counter{0};
+  std::array<int, 2> dim{};
+  std::size_t nnzInE{};
 
-        SchurHEntrance() = default;
+  SchurHEntrance() = default;
 
-        void buildRandomAccess();
-    };
-}
+  void buildRandomAccess();
+};
+}  // namespace MegBA
